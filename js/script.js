0e2f08b86a8d310863a6b88db8502e8f6afb73aa
Carmine Bonavoglia <cogitoergosumpoco@gmail.com>
2021-04-15 19:17:08 +0200

    Vue

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+
+    data: {
+
+    logoHeader: 'finance_logo_1x.png',
+
+    logoFooter:'finance_logo_footer.png',
+    
+    headerMenu: [
+        'Home',
+        'About',
+        'Services',
+        'Packages',
+        'Insight'
+    ],
+
+    recent:[
+        'When is it the right time to sell your company?',
+        'Personal debt or company debt we explore your options',
+        'An interview with a so-called business angel'
+    ],
+
+    services:[
+        'SMALL BUSINESS LOAN SERVICES',
+        'CREDIT RATING ADVICE SERVICES',
+        'CROWD FUNDING SERVICES'
+    ]},
+
+});
